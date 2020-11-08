5544e8a41b035068b68bf4e1bd66d6b21c0b8a62
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-11-08 14:32:06 -0500

    Good Copy

--- scripts/assign4.js
+++ scripts/assign4.js
@@ -1,19 +1,28 @@
 ﻿//#### ASSIGNMENT 4 - VETERINARY CLINIC ####
-//pet objects
+//STUDENT: BRYAN HUGHES
+//SECTION: B
+
+/********************
+*   Pet Object #1   *
+*********************/
 var animalOne = {
     type: 'Cat',
     name: 'Mary Jane',
     breed: 'Calico'
 };
 
+/********************
+*   Pet Object #2   *
+*********************/
 var animalTwo = {
     type: 'Dog',
     name: 'Bruce',
     breed: 'Golden Doodle'
 };
 
-//client object
-
+/********************
+*   Client Object   *
+*********************/
 var client = {
     clientName: 'Bryan Hughes',
     phone: '647-999-9292',
@@ -21,22 +30,46 @@ var client = {
     vetClinic: 'Westbridge Veterinary Hospital'
 };
 
-//Logical programming
+/**********************
+* Logical programming *
+***********************/
 
-window.addEventListener("load", addClientName);
-var spanBtn = document.getElementById('clientBtn');
-window.addEventListener("click", showPets);
+//When the window loads, fires the 'documentReady' function
+window.onload = documentReady;
+
+/************************************************
+   A function with all the page's methods inside
+*************************************************/
+function documentReady() {
+
+    //Listens for the html page to load and then calls the addClientName function
+    window.addEventListener("load", addClientName);
+
+    /***************************************
+        Places content into 'clientName' span
+    ****************************************/
+    function addClientName() {
+        var spanElement = document.getElementById('clientName');
+        spanElement.innerHTML = client.clientName;
+    }
 
 
-function addClientName() {
-    var spanElement = document.getElementById('clientName');
-    spanElement.innerHTML = client.clientName;
-}
+    //Stores the HTML element 'clientBtn' as variable
+    var showPetsBtn = document.getElementById('clientBtn');
 
-function showPets() {
-    var petOne = document.getElementById('pet1');
-    petOne.innerHTML = animalOne.name + " ( " + animalOne.type + " )";
+    //Listening for the click event for the  'showPetsBtn'
+    showPetsBtn.addEventListener("click", showPets);
 
-    var petTwo = document.getElementById('pet2');
-    petTwo.innerHTML = animalTwo.name + " ( " + animalTwo.type + " )";
+    /*****************************************
+       Populates the 'pet1' and 'pet2' span
+       with the first Pet object info and the
+       second Pet object info
+    ******************************************/
+    function showPets() {
+        var petOne = document.getElementById('pet1');
+        petOne.innerHTML = `${client.pets[0]} ( ${animalOne.type} )`;
+
+        var petTwo = document.getElementById('pet2');
+        petTwo.innerHTML = `${client.pets[1]} ( ${animalTwo.type} )`;
+    }
 }
